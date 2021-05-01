62dfa6bfc08628f57d255735868fbd532a593670
Vanessa França <vafranca@gmail.com>
2021-05-01 17:07:01 -0300

    Colocar usuario em uma pasta

---
+++ usuario/novo_usuario.php
@@ -0,0 +1,38 @@
+<?php
+$page_title = "Demo : Inserção de Usuário";
+// layout do cabeçalho
+include_once "../header.php";
+ ?>
+ <main role="main" class="container">
+    <h3 class="mb-3">Novo Usuário</h3>
+    <div class="row">
+        <form action="insere_usuario.php" method="post">
+            <table class='table table-hover table-responsive table-bordered'>
+                <tr>
+                    <td>Email</td>
+                    <td><input type='email' name='email' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Senha</td>
+                    <td><input type='text' name='senha' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Nome</td>
+                    <td><input type='text' name='nome' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <button type="submit" class="btn btn-primary">Inserir</button>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</main>
+<?php
+// layout do rodapé
+include_once "../footer.php";
+?>
+
+

51777897085587cf334de9f0a1c85b8443af1369
Vanessa França <vafranca@gmail.com>
2021-05-02 18:15:34 -0300

    Atualizar dump banco

--- web2.sql
+++ web2.sql
@@ -17,30 +17,12 @@ CREATE TABLE `produto` (
   `nome` varchar(255) NOT NULL,
   `descricao` text NOT NULL,
   `foto` varchar(255) NOT NULL,
-  `fornecedor_id` int NOT NULL,
+  `fornecedor_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fornecedor_id` (`fornecedor_id`),
   CONSTRAINT `produto_ibfk_1` FOREIGN KEY (`fornecedor_id`) REFERENCES `fornecedor` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `produto` (`id`, `nome`, `descricao`, `foto`, `fornecedor_id`) VALUES
-(5,	'Teste',	'erere',	'',	1),
-(6,	'Guarda Chuva Rainbow',	'teste',	'',	1);
-
-DROP TABLE IF EXISTS `estoque`;
-CREATE TABLE `estoque` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `quantidade` int NOT NULL,
-  `preco` decimal(12,2) NOT NULL,
-  `produto_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `produto_id` (`produto_id`),
-  CONSTRAINT `estoque_ibfk_1` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-INSERT INTO `estoque` (`id`, `quantidade`, `preco`, `produto_id`) VALUES
-(1,	20,	56.23,	5),
-(5,	30,	89.90,	6);
 
 DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
@@ -56,4 +38,15 @@ INSERT INTO `usuario` (`id`, `email`, `senha`, `nome`) VALUES
 (3,	'vafranca@gmail.com',	'25c1c47fa6224632c6dc07137ea6089a',	'Vanessa'),
 (4,	'doug@doug.com',	'b8c5fa008715b53b95c874d73b4dcc02',	'Douglas');
 
--- 2021-05-02 16:23:18
+DROP TABLE IF EXISTS `estoque`;
+CREATE TABLE `estoque` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `quantidade` int NOT NULL,
+  `preco` decimal(12,2) NOT NULL,
+  `produto_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `produto_id` (`produto_id`),
+  CONSTRAINT `estoque_ibfk_1` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- 2021-05-02 21:14:54
